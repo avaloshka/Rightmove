6b6bca1e6d8feefa7483a8b0c6873ef63b643fa6
Alex Valoshka <alexvaloshka@gmail.com>
2022-12-27 12:41:20 +0000

    changed parsing class

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Rightmove:
     results = []
     void = False
     # Change the number in production for actual number of pages that needs to be scraped
-    total_pages = 42
+    total_pages = 8
 
     def fetch(self, url):
         print(f"HTTP request to URL: {url}", end="")
@@ -26,7 +26,9 @@ class Rightmove:
         titles = [title.text.strip() for title in content.findAll('h2', {'class': 'propertyCard-title'})]
         addresses = [address.text.strip() for address in content.findAll('address', {'class': 'propertyCard-address'})]
         descriptions = [description.text.strip() for description in content.findAll('span', {'itemprop': 'description'})]
-        prices = [price.text.strip() for price in content.findAll('div', {'class': 'propertyCard-priceValue'})]
+        prices = [price.text.strip() for price in content.findAll('div', {'class': 'propertyCard-rentalPrice-primary'})]
+        for price in prices:
+            print(price)
         dates = [date.text.split() for date in content.findAll('span', {'class': 'propertyCard-branchSummary-addedOrReduced'})]
         # split dates and get last value in the list- the actual date
         d = []
@@ -54,25 +56,21 @@ class Rightmove:
                 'image': images[index],
             }
             self.results.append(item)
+        print('Items are written to self.results')
 
     def to_csv(self):
         df = pd.DataFrame(self.results)
         df.to_csv("rightmove.csv")
-
-        # with open('rightmove.csv', 'w') as csv_file:
-        #     writer = csv.DictWriter(csv_file, fieldnames=self.results[0].keys())
-        #
-        #     for row in self.results:
-        #         writer.writerow(row)
-        #     print('Results are stored in "rightmove.csv" ')
+        print("Saved to csv")
 
     def run(self):
 
         for page in range(0, self.total_pages):
             try:
-                index = page * 25
+                index = page * 24
                 # url = f'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E87490&sortType=1&index={index}&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords='
-                url = 'https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E182&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&includeSSTC=true&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false'
+                # url = 'https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E182&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&includeSSTC=true&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false'
+                url = f'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E182&index={index}&insId=1&radius=0.0&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='
                 response = self.fetch(url)
 
                 self.parse(response.text)
